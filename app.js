c2410b112aac0fe8f5f6d6774fd646a1479ea511
42b883 <42b883@gmail.com>
2018-11-29 16:15:44 +0600

    game initialization

--- app.js
+++ app.js
@@ -12,9 +12,8 @@ GAME RULES:
 
 let scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+init();
+
 
 dice = Math.floor(Math.random() * 6) + 1;
 
@@ -24,12 +23,7 @@ dice = Math.floor(Math.random() * 6) + 1;
 //let x = document.querySelector('#score-0').textContent;
 //console.log(x);
 
-document.querySelector('.dice').style.display = 'none';
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     //1. random number
@@ -84,8 +78,29 @@ function nextPlayer() {
 
    document.querySelector('.dice').style.display = 'none';
 }
+    document.querySelector('.btn-new').addEventListener('click', init);
+    function init() {
+        scores = [0, 0];
+        activePlayer = 0;
+        roundScore = 0;
+
+        document.querySelector('.dice').style.display = 'none';
+
+        document.getElementById('score-0').textContent = '0';
+        document.getElementById('score-1').textContent = '0';
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        document.getElementById('name-0').textContent = 'Player 0';
+        document.getElementById('name-1').textContent = 'Player 1';
+        document.querySelector('.player-0-panel').classList.remove = 'winner';
+        document.querySelector('.player-1-panel').classList.remove = 'winner';
+        document.querySelector('.player-0-panel').classList.remove  = 'active';
+        document.querySelector('.player-1-panel').classList.remove = 'active';
+        document.querySelector('.player-0-panel').classList.add = 'active';
 
 
+
+    }
 
 
 
